2a3283c18e500f1b38ab5e63726c9b3733ca3051
Niel <niel@localhost.localdomain>
2017-12-18 22:36:09 +0100

    Some more documentation added to classes

--- ChocolateShot.py
+++ ChocolateShot.py
@@ -1,19 +1,26 @@
 class ChocolateShot:
+    #Constructor
+    #param type: Type of choclateshot
+    #param expiredate: The expirationdate of the shot
     def __init__(self, type, expiredate):
         self.type = type
         self.price = 1
         self.expiredate = expiredate
-
+    
+    #Destructor
     def __del__(self):
         self.type = None
         self.price = None
         self.expiredate = None
 
+    #Returns the type of this shot
     def getType(self):
         return self.type
-
+    
+    #Returns the price of this shot
     def getPrice(self):
         return self.price
 
+    #Returns the expirationdate of this shot
     def getExpiredate(self):
         return self.expiredate
