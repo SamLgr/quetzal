0d99fa71290f1bb54bfc405e583208e26294e83b
Niel <niel@localhost.localdomain>
2017-12-19 23:15:22 +0100

    bug fix

--- Stock.py
+++ Stock.py
@@ -60,43 +60,67 @@ class Stock:
     def stockOrder(self, stockType, amount):
         if stockType == "white chocolate":
             while amount != 0:
-                chocolate = ChocolateShot("white")
+                chocolate = ChocolateShot("white", 5)
                 self.chocolatestock.tableInsert(chocolate)
+                amount-=1
         elif stockType == "milk chocolate":
             while amount != 0:
-                chocolate = ChocolateShot("milk")
+                chocolate = ChocolateShot("milk", 5)
                 self.chocolatestock.tableInsert(chocolate)
+                amount-=1
         elif stockType == "brown chocolate":
             while amount != 0:
-                chocolate = ChocolateShot("brown")
+                chocolate = ChocolateShot("brown", 5)
                 self.chocolatestock.tableInsert(chocolate)
+                amount-=1
         elif stockType == "black chocolate":
             while amount != 0:
-                chocolate = ChocolateShot("black")
+                chocolate = ChocolateShot("black", 5)
                 self.chocolatestock.tableInsert(chocolate)
+                amount-=1
         elif stockType == "honey":
             while amount != 0:
-                honey = Honey()
+                honey = Honey(5)
                 self.honeystock.tableInsert(honey)
+                amount-=1
         elif stockType == "marshmallow":
             while amount != 0:
-                marshmallow = Marshmallow()
+                marshmallow = Marshmallow(5)
                 self.marshmallowstock.tableInsert(marshmallow)
+                amount-=1
         elif stockType == "chilipepper":
             while amount != 0:
-                chilipepper = ChiliPepper()
+                chilipepper = ChiliPepper(5)
                 self.chilipepperstock.tableInsert(chilipepper)
+                amount-=1
 
     #Function to check if the amount in a specific stock is to low
     #If it is to low, order more
     def stockCheck(self):
         #ChocolateStock
-        length = self.chocolatestock.getLength()
-        if length < 2:
-            self.stockOrder("white chocolate", 5)
+        traverselist = self.chocolatestock.traverseTable()
+        milkcounter = 0
+        whitecounter = 0
+        blackcounter = 0
+        browncounter = 0
+        for choco in traverselist:
+            if choco.getType() == "milk chocolate":
+                milkcounter += 1
+            if choco.getType() == "white chocolate":
+                whitecounter += 1
+            if choco.getType() == "black chocolate":
+                blackcounter += 1
+            if choco.getType() == "brown chocolate":
+                browncounter += 1
+
+        if milkcounter < 2:
             self.stockOrder("milk chocolate", 5)
-            self.stockOrder("brown chocolate", 5)
+        if whitecounter < 2:
+            self.stockOrder("white chocolate", 5)
+        if blackcounter < 2:
             self.stockOrder("black chocolate", 5)
+        if browncounter < 2:
+            self.stockOrder("brown chocolate", 5)
 
         #HoneyStock
         length = self.honeystock.getLength()
@@ -116,7 +140,31 @@ class Stock:
         return True
             
 """
->>>s = Stock()
->>>s.stockCheck()
+        >>> t = StockTable()
+        >>> h = Honey(5)
+        >>> h2 = Honey(2)
+        >>> t.tableInsert(h)
+        True
+        >>> t.tableInsert(h2)
+        True
+        >>> t.tableInsert(h2)
+        True
+        >>> s = Stock()
+        >>> s.stockCheck()
 True
 """
+
+t = StockTable()
+c = ChocolateShot("milk chocolate", 5)
+c3 = ChocolateShot("milk chocolate", 5)
+c4 = ChocolateShot("milk chocolate", 5)
+c2 = ChocolateShot("brown chocolate", 5)
+
+t.tableInsert(c)
+t.tableInsert(c2)
+t.tableInsert(c3)
+t.tableInsert(c4)
+
+s = Stock()
+s.setChocolatestock(t)
+s.stockCheck()
