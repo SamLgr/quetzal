89a901e6596b6b474a57447204040b36ef5b9433
Lander De Roeck <lander.deroeck@student.uantwerpen.be>
2017-12-19 18:25:11 +0100

    adding support for orderitem

--- ChocolateMilk.py
+++ ChocolateMilk.py
@@ -1,31 +1,29 @@
 class ChocolateMilk:
-    #Constructor
-    #param id: ID of this chocolatemilk
     def __init__(self, id):
         self.id = id
         self.price = 2
-        self.ingredients = None
+        self.ingredients = {}
+        self.workload = 5
 
-    #Destructor
     def __del__(self):
         self.id = None
         self.price = None
         self.ingredients = None
 
-    #Returns the price of this chocolatemilk
     def getPrice(self):
         return self.price
 
-    #Returns the price of this chocolatemilk
     def getId(self):
         return self.id
 
-    #Adds ingredients to this chocolatemilk
-    def addIngredient(self):
-<<<<<<< HEAD
-    
-    #Returns the amount of workload this chocolatemilk takes
-=======
+    def addIngredient(self, ingredient, amount):
+        if self.ingredients[ingredient] is None:
+            self.ingredients[ingredient]=amount
+        elif self.ingredients[ingredient] is not None:
+            self.ingredients += amount
+        self.price += ingredient.price
+        self.workload += amount
 
->>>>>>> b445e1b6d83a6626819229d6e55c99571b1191ed
     def returnWorkload(self):
+        return self.workload
+
