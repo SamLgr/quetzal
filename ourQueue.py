fed78446814f47f7a423428fc29f809147ed2778
Niel <niel@localhost.localdomain>
2018-01-20 16:24:05 +0100

    Traverse added to ourQueue

--- ourQueue.py
+++ ourQueue.py
@@ -45,3 +45,23 @@ class Queue(object):
         if self.isEmpty():
             return None, False
         return self.frontPtr.getItem(), True
+
+    def traverse(self):
+        """
+        >>> q = Queue()
+        >>> q.enqueue("A")
+        >>> q.enqueue("B")
+        >>> q.enqueue("C")
+        >>> q.enqueue("D")
+        >>> q.traverse()
+        ['A', 'B', 'C', 'D']
+        """
+
+        buffer = []
+        current = self.frontPtr
+        buffer.append(current.item)
+        while (current.getNext() != None):
+            current = current.getNext()
+            buffer.append(current.item)
+
+        return buffer
